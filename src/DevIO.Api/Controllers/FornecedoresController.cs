9e4b05e27795f4cbd1f1092f4f93ade1944981fc
Lorram Nascimento <lorramrj@gmail.com>
2022-04-23 12:10:16 -0300

    Created MainController e FornecedoresController

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IMapper _mapper;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository,
+                                      IMapper mapper)
+        {
+            _fornecedorRepository = fornecedorRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
+        {
+            var fornecedores = _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
+            return fornecedores;
+        }
+    }
+}
