fe34c9d9aff3010aa49c9aee49f7ae6d3ba8eaa7
Lorram Nascimento <lorramrj@gmail.com>
2022-04-27 12:03:37 -0300

    Configuring Identity in the Project and its Context

---
+++ src/DevIO.Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,27 @@
+﻿using DevIO.Api.Data;
+using DevIO.Api.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DevIO.Api.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services,
+          IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
